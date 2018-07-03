3bdf6e0a540175f3b45a12da336c2de1d10009f8
fabricio <fabricio.fvs@gmail.com>
2018-07-02 22:09:01 -0300

    primeiro commit

---
+++ app/src/main/java/digitalhouse/com/exercicioaula24/MainActivity02.java
@@ -0,0 +1,13 @@
+package digitalhouse.com.exercicioaula24;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity02 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main02);
+    }
+}
---
+++ app/src/main/java/digitalhouse/com/exercicioaula24/MainActivity_HorarioDosPaises.java
@@ -0,0 +1,13 @@
+package digitalhouse.com.exercicioaula24;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity_HorarioDosPaises extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main__horario_dos_paises);
+    }
+}
